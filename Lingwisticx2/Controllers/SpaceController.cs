7f6c2356cd69fc83587fba1a95f7dc05c896889d
dnkFrvgv <carinefrvga@gmail.com>
2024-02-21 23:03:32 -0300

    Add project files.

---
+++ Lingwisticx2/Controllers/SpaceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lingwisticx2.Controllers
+{
+    public class SpaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Vocabulary()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lingwisticx2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lingwisticx2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
